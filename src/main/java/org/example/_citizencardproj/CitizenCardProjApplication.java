75257ad0234527d2756037d58b2487dcf2bf4ded
alfo0924 <halo0924@yahoo.com.tw>
2024-10-18 17:09:38 +0800

    v1.01 init cnp

---
+++ src/main/java/org/example/_citizencardproj/CitizenCardProjApplication.java
@@ -0,0 +1,13 @@
+package org.example._citizencardproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenCardProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitizenCardProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/_citizencardproj/CitizenCardProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._citizencardproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenCardProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
